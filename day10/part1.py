bdada581e73a634349a2e21dae8c4122db6d27e0
Eduardo Chapa <eduardo.chapa@gmail.com>
2023-12-16 18:09:52 -0600

    Day 10, part 1.

--- day10/part1.py
+++ day10/part1.py
@@ -45,17 +45,14 @@ def count_steps(pipes: list, start: (int, int)) -> int:
     height = len(pipes)
     width = len(pipes[0])
     steps = 0
-    def flood_fill_pipes(pipes: list, x: int, y: int, dir: str,
-                         start: (int, int), steps: int) -> int:
+    def flood_fill_pipes(x: int, y: int, dir: str, steps: int) -> int:
         """ Recursively follow the pipes and return the steps required to
         fill the loop from the start.
 
         Args:
-            pipes (list): A list of lists with all the pipes.
             x (int): The current position's x.
             y (int): The current position's y.
             dir (str): Previous call's direction.
-            start (int, int): The starting point.
             steps (int): The number of steps at this position.
 
         Returns:
@@ -67,55 +64,42 @@ def count_steps(pipes: list, start: (int, int)) -> int:
             match dir:
                 case 'up':
                     if pipes[y][x] == '|':
-                        return flood_fill_pipes(pipes, x, y-1,
-                                                'up', start, steps+1)
+                        return flood_fill_pipes(x, y-1, 'up', steps+1)
                     elif pipes[y][x] == 'F':
-                        return flood_fill_pipes(pipes, x+1, y,
-                                                'right', start, steps+1)
+                        return flood_fill_pipes(x+1, y, 'right', steps+1)
                     elif pipes[y][x] == '7':
-                        return flood_fill_pipes(pipes, x-1, y,
-                                                'left', start, steps+1)
+                        return flood_fill_pipes(x-1, y, 'left', steps+1)
                 case 'down':
                     if pipes[y][x] == '|':
-                        return flood_fill_pipes(pipes, x, y+1,
-                                                'down', start, steps+1)
+                        return flood_fill_pipes(x, y+1, 'down', steps+1)
                     elif pipes[y][x] == 'L':
-                        return flood_fill_pipes(pipes, x+1, y,
-                                                'right', start, steps+1)
+                        return flood_fill_pipes(x+1, y, 'right', steps+1)
                     elif pipes[y][x] == 'J':
-                        return flood_fill_pipes(pipes, x-1, y,
-                                                'left', start, steps+1)
+                        return flood_fill_pipes(x-1, y, 'left', steps+1)
                 case 'left':
                     if pipes[y][x] == '-':
-                        return flood_fill_pipes(pipes, x-1, y,
-                                                'left', start, steps+1)
+                        return flood_fill_pipes(x-1, y, 'left', steps+1)
                     elif pipes[y][x] == 'F':
-                        return flood_fill_pipes(pipes, x, y+1,
-                                                'down', start, steps+1)
+                        return flood_fill_pipes(x, y+1, 'down', steps+1)
                     elif pipes[y][x] == 'L':
-                        return flood_fill_pipes(pipes, x, y-1,
-                                                'up', start, steps+1)
+                        return flood_fill_pipes(x, y-1, 'up', steps+1)
                 case 'right':
                     if pipes[y][x] == '-':
-                        return flood_fill_pipes(pipes, x+1, y,
-                                                'right', start, steps+1)
+                        return flood_fill_pipes(x+1, y, 'right', steps+1)
                     elif pipes[y][x] == 'J':
-                        return flood_fill_pipes(pipes, x, y-1,
-                                                'up', start, steps+1)
+                        return flood_fill_pipes(x, y-1, 'up', steps+1)
                     elif pipes[y][x] == '7':
-                        return flood_fill_pipes(pipes, x, y+1,
-                                                'down', start, steps+1)
-
+                        return flood_fill_pipes(x, y+1, 'down', steps+1)
 
     y, x = start
     if y-1 >= 0 and pipes[y-1][x] in ['|', 'F', '7']:
-        steps = flood_fill_pipes(pipes, x, y-1, 'up', start, 1)
+        steps = flood_fill_pipes(x, y-1, 'up', 1)
     elif y+1 <= height-1 and pipes[y+1][x] in ['|', 'L', 'J']:
-        steps = flood_fill_pipes(pipes, x, y+1, 'down', start, 1)
+        steps = flood_fill_pipes(x, y+1, 'down', 1)
     elif x-1 >= 0 and pipes[y][x-1] in ['-', 'F', 'L']:
-        steps = flood_fill_pipes(pipes, x-1, y, 'left', start, 1)
+        steps = flood_fill_pipes(x-1, y, 'left', 1)
     elif x+1 <= width-1 and pipes[y][x+1] in ['-', 'J', '7']:
-        steps = flood_fill_pipes(pipes, x+1, y, 'right', start, 1)
+        steps = flood_fill_pipes(x+1, y, 'right', 1)
     return steps
 
 
